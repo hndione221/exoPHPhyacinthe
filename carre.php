87d864f210fe2f3ff211c245545bd4d57a3b2b03
hndione221 <77298232+hndione221@users.noreply.github.com>
2021-01-11 22:43:41 +0000

    Add files via upload

---
+++ carre.php
@@ -0,0 +1,18 @@
+<?php
+    define("cote",2);
+    echo cote;
+    $c1 = 2;
+    $c2 = 2;
+    $c3 = 2;
+    $c4 = 2;
+    #calcul du perimetre
+    echo (c1 + c2 + c3 + c4);
+    # demi preimetre
+    echo (c1 + c2);
+    # surface
+    echo (c1 * c2);
+    
+
+
+
+?>
---
+++ rectangle.php
@@ -0,0 +1,11 @@
+<?php
+        $L = 10;
+        $l = 20;
+        echo $L; echo "<br\>";
+        echo $l;
+        #perimetre
+        echo ($l + $L) * 2;
+        #demiperimetre
+        echo ($l + $L);
+        
+?>
