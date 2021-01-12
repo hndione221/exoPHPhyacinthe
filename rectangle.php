63e03332139d7949b913c339f36eead8683c7060
hndione221 <77298232+hndione221@users.noreply.github.com>
2021-01-12 14:47:00 +0000

    Update rectangle.php

--- rectangle.php
+++ rectangle.php
@@ -1,11 +1,13 @@
 <?php
-        $L = 10;
-        $l = 20;
-        echo $L; echo "<br\>";
-        echo $l;
-        #perimetre
-        echo ($l + $L) * 2;
-        #demiperimetre
-        echo ($l + $L);
+      $L =20;
+      $l=10;
+      $perimetre= (($L + $l) * 2);
+      $demiPerimetre = $perimetre / 2;
+      $surface = $L * $l;
+      $diagonale = sqrt (($L**2) + ($l**2));
+      echo "le perimetre est $perimetre"echo "<br/>";
+      echo "le demi perimetre est $demiPerimetre"echo "<br/>";
+      echo "la surface est $surface"echo "<br/>";
+      echo "la diagonale est $diagonale";
         
 ?>
